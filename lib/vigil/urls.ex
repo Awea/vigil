f060a148753a06d6000f60fd2bd33c675a532c74
Awea <aweaoftheworld@gmail.com>
2017-12-04 17:06:57 +0100

    :sparkles: Send urls one time

--- lib/vigil/urls.ex
+++ lib/vigil/urls.ex
@@ -5,6 +5,8 @@ defmodule Vigil.Urls do
 
   alias Exredis.Api, as: Repo
 
+  require Logger
+
   @doc """
   Return a List of urls.
   """
@@ -14,18 +16,24 @@ defmodule Vigil.Urls do
   end
 
   @doc """
-  Remove urls.
+  Remove urls and store removed to url-sent.
   """
   def delete_all do
+    all 
+    |> Enum.map(&Repo.set("url-sent:#{&1}", &1))
+
     urls_keys
     |> Enum.each(&Repo.del/1)
   end
 
   @doc """
-  Store a new url.
+  Store a new url if doesn't exist in url-sent:*.
   """
   def add(value) do
-    Repo.set("url:#{value}", value)
+    case Repo.get("url-sent:#{value}") do
+      :undefined -> Repo.set("url:#{value}", value)
+      _          -> Logger.info "url already sent"
+    end
   end
 
   defp urls_keys do
