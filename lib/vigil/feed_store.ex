7af7b5c9d10bfadf9098eebe55b2c97d7d768940
Awea <aweaoftheworld@gmail.com>
2018-12-19 13:48:29 +0100

    :sparkles: Send an email when github token is unvalid

--- lib/vigil/feed_store.ex
+++ lib/vigil/feed_store.ex
@@ -3,17 +3,32 @@ defmodule Vigil.FeedStore do
   This module get an XML feed from GitHub, filter it, extract urls, then store them in Redis.
   """
 
+  import Bamboo.Email
+
   alias Vigil.Urls
 
   def process_feed do
     github_feed()
     |> HTTPotion.get
-    |> Map.get(:body)
-    |> FeederEx.parse 
+    |> get_parsed_response
     |> filter_feed 
     |> format_feed
     |> Enum.each(&(Urls.add/1))
   end
+
+  defp get_parsed_response(%{status_code: 401}) do
+    new_email(
+      to: "david@wearemd.com",
+      from: "bot@wearemd.com",
+      subject: "Github feed error",
+      html_body: "<p>Please update github.token in config/secrets.exs</p>",
+      text_body: "see this in html"
+    )
+    |> Vigil.Mailer.deliver_now
+
+    :error
+  end
+  defp get_parsed_response(%{body: body, status_code: 200}), do: FeederEx.parse(body)
 
   # Filter the feed by removing WatchEvent and some users:
   # wearemd, awea, mmaayylliiss
@@ -22,8 +37,10 @@ defmodule Vigil.FeedStore do
       String.contains?(entry.id, "WatchEvent") && !String.contains?(entry.link, ["wearemd", "awea", "mmaayylliiss"])
     end)
   end
+  defp filter_feed(_), do: :error
 
   # Format feed to a List of Urls
+  defp format_feed(:error), do: []
   defp format_feed(entries) do
     Enum.map(entries, &(&1.link))
   end
