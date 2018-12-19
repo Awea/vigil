c02992079df6d0059cdca64429a0179550870a1b
Awea <aweaoftheworld@gmail.com>
2018-12-19 13:43:40 +0100

    :sparkles: Update to Elixir 1.7.4

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Vigil.Mixfile do
     [
       app: :vigil,
       version: @version,
-      elixir: "~> 1.5.2",
+      elixir: "~> 1.7.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps()
